72e35f61b839d121d9e3b9dc2dc99a47b03ca7e0
leandrodcs <eng.leandrodcs@gmail.com>
2021-12-15 16:51:27 -0300

    Attempting to fix heroku H10 error

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,7 @@
 /* eslint-disable no-console */
 import app, { init } from './app';
 
-const port = +process.env.PORT || 4000;
+const port = process.env.PORT || 4000;
 
 init().then(() => {
     app.listen(port, () => {
