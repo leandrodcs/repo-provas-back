9a9606570d54306dc7239ff612aa3a08ed06205a
leandrodcs <eng.leandrodcs@gmail.com>
2021-12-17 18:40:47 -0300

    Subjects now come with the amout of exams each has

--- src/services/subjectService.ts
+++ src/services/subjectService.ts
@@ -1,7 +1,18 @@
 import * as subjectRepository from '../repositories/subjectRepository';
+import * as examRepository from '../repositories/examRepository';
 
 async function listSubjects(courseId: number) {
-    const subjects = await subjectRepository.listSubjects(courseId);
+    const result = await subjectRepository.listSubjects(courseId);
+
+    const exams = await examRepository.getAllExams();
+    const subjects = [];
+
+    for (let i = 0; i < result.length; i++) {
+        subjects.push({
+            ...result[i],
+            examCount: exams.filter((exam) => exam.subjectId === result[i].id).length,
+        });
+    }
 
     return subjects;
 }
