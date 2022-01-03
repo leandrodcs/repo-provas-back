1ae0409fbe3d7de6c413e6214611f2430d1638e0
ligia.bozzi@gmail.com <ligia@respondeai.com.br>
2022-01-03 09:39:14 -0300

    preparing review

--- src/routers/courseRouter.ts
+++
@@ -1,8 +0,0 @@
-import express from 'express';
-import * as courseController from '../controllers/courseController';
-
-const router = express.Router();
-
-router.get('', courseController.listCourses);
-
-export default router;
--- tests/utils/database.ts
+++
@@ -1,7 +0,0 @@
-import { getRepository } from 'typeorm';
-
-import Exam from '../../src/entities/Exam';
-
-export async function clearDatabase() {
-    await getRepository(Exam).delete({});
-}
